439f0bc145d128d7c805d8dfd710f0dda480a741
sunwei <sunwei9366@gmail.com>
2020-12-22 18:31:57 +0800

    update store and reducer

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,13 @@
+
+export const initialState = {
+    posts: [],
+    loading: false,
+    hasErrors: false,
+  }
+  
+  export default function postsReducer(state = initialState, action) {
+    switch (action.type) {
+      default:
+        return state
+    }
+}
